d4cfdffc6db55fce4dc88b26de9beb2c09022561
Cournflaeik <elien.degeetere@gmail.com>
2022-10-20 04:03:53 +0200

    Feat: add threejs and scene

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
